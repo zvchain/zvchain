65ceccaba1c39a7e47ad908da774d3d297b7dff9
sesamegu <haiquan81@163.com>
2019-06-19 19:52:09 +0800

    remove unused method

--- network/interface.go
+++ network/interface.go
@@ -104,23 +104,14 @@ type Network interface {
 	// send message to the guys which belongs to the same group with the node and they will rely the message to the node
 	SendWithGroupRelay(id string, groupID string, msg Message) error
 
-	//Random broadcast the message to parts nodes in the group which self belongs to
-	RandomSpreadInGroup(groupID string, msg Message) error
-
 	//Broadcast the message among the group which self belongs to
 	SpreadAmongGroup(groupID string, msg Message) error
-
-	//SpreadToRandomGroupMember send message to random memebers which in special group
-	SpreadToRandomGroupMember(groupID string, groupMembers []string, msg Message) error
 
 	//SpreadToGroup Broadcast the message to the group which self do not belong to
 	SpreadToGroup(groupID string, groupMembers []string, msg Message, digest MsgDigest) error
 
 	//TransmitToNeighbor Send message to neighbor nodes
 	TransmitToNeighbor(msg Message) error
-
-	//Relay Send the message to part nodes it connects to and they will also broadcast the message to part of their neighbor util relayCount
-	Relay(msg Message, relayCount int32) error
 
 	//Broadcast Send the message to all nodes it connects to and the node which receive the message also broadcast the message to their neighbor once
 	Broadcast(msg Message) error
