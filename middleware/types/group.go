db4e539f8998985dc551053617226ff9e929b2bc
darren0718 <kobep@126.com>
2019-07-04 16:26:28 +0800

    interface updated

--- middleware/types/group.go
+++ middleware/types/group.go
@@ -22,29 +22,20 @@ type SeedI interface {
 	Seed() common.Hash
 }
 
-// Piece明文数据包接口
-type SenderPiece interface {
-	Receiver() []byte  // piece的接收者
-	PieceData() []byte // Piece明文
-}
-type SenderPiecePacket interface {
+type SharePiecePacket interface {
 	SeedI
-	Sender() []byte        //发送者
-	Pieces() []SenderPiece //发送者对组内每个人的明文分片
+	Sender() []byte
+	Pieces() []byte // Encrypted pieces data
+}
+type EncryptedSharePiecePacket interface {
+	SharePiecePacket
+	Pubkey0() []byte // Initial Pubkey
 }
 
-type EncryptedSenderPiecePacket interface {
-	SenderPiecePacket
-	Pubkey0() []byte
+type OriginSharePiecePacket interface {
+	SharePiecePacket
+	EncSeckey() []byte
 }
-
-type EncryptedReceiverPiece interface {
-	Sender() []byte    // piece的发送者
-	PieceData() []byte // Piece加密后的数据
-	Pubkey0() []byte   //piece的发送者的公钥
-}
-
-type OriginSenderPiecePacket SenderPiecePacket
 
 // Mpk数据包接口
 type MpkPacket interface {
@@ -57,7 +48,7 @@ type MpkPacket interface {
 // 建组数据接口
 type FullPacket interface {
 	Mpks() []MpkPacket
-	Pieces() []EncryptedSenderPiecePacket
+	Pieces() []EncryptedSharePiecePacket
 }
 
 type MemberI interface {
@@ -100,7 +91,7 @@ type GroupCreateChecker interface {
 
 	// 校验一个piece交易是否合法，如果合法，则返回该加密后的piece数据
 	// 链需要把piece数据存储到db
-	CheckEncryptedPiecePacket(packet EncryptedSenderPiecePacket, ctx CheckerContext) error
+	CheckEncryptedPiecePacket(packet EncryptedSharePiecePacket, ctx CheckerContext) error
 
 	// 校验一个mpk交易是否合法，如果合法，则返回mpk数据
 	CheckMpkPacket(packet MpkPacket, ctx CheckerContext) error
@@ -110,7 +101,7 @@ type GroupCreateChecker interface {
 	CheckGroupCreateResult(ctx CheckerContext) (resultCode int, data interface{}, err error)
 
 	// 检查origin piece
-	CheckOriginPiecePacket(packet OriginSenderPiecePacket, ctx CheckerContext) error
+	CheckOriginPiecePacket(packet OriginSharePiecePacket, ctx CheckerContext) error
 
 	// 检查惩罚
 	CheckGroupCreatePunishment(ctx CheckerContext) (PunishmentMsg, error)
@@ -121,20 +112,25 @@ type GroupStoreReader interface {
 
 	// 返回指定seed的piece数据
 	//  共识通过此接口获取所有piece数据，生成自己的签名私钥和签名公钥
-	GetEncryptedReceiverPiece(receiver []byte, seed SeedI) ([]EncryptedReceiverPiece, error)
+	GetEncryptedPiecePackets(seed SeedI) ([]EncryptedSharePiecePacket, error)
 
 	// 查询指定sender 和seed 是否有piece数据
-	HasSentEncryptedPiecePacket(sender []byte) bool
+	HasSentEncryptedPiecePacket(sender []byte, seed SeedI) bool
+
+	// 查询是否已发送过mpk包
+	HasSentMpkPacket(sender []byte, seed SeedI) bool
 
 	// 返回所有的建组数据
 	// 共识在校验是否建组成时调用
-	GetPieceAndMpkPackets(seed SeedI) (FullPacket, error)
+	GetMpkPackets(seed SeedI) (FullPacket, error)
 
 	// 返回origin piece 是否需要的标志
 	IsOriginPieceRequired(seed SeedI) bool
 
 	// 获取所有明文分片
-	GetAllOriginPiecePackets(seed SeedI) ([]OriginSenderPiecePacket, error)
+	GetOriginPiecePackets(seed SeedI) ([]OriginSharePiecePacket, error)
+
+	HasSentOriginPiecePacket(sender []byte, seed SeedI) bool
 
 	// Get available group infos at the given height
 	GetAvailableGroupInfos(h uint64) []GroupI
@@ -144,11 +140,11 @@ type GroupStoreReader interface {
 type GroupPacketSender interface {
 
 	// 发送加密piece分片
-	SendEncryptedPiecePacket(packet EncryptedSenderPiecePacket) error
+	SendEncryptedPiecePacket(packet EncryptedSharePiecePacket) error
 
 	//发送签名公钥包
 	SendMpkPacket(packet MpkPacket) error
 
 	// 发送明文piece包
-	SendOriginPiecePacket(packet OriginSenderPiecePacket) error
+	SendOriginPiecePacket(packet OriginSharePiecePacket) error
 }
