591957cb1ea9eb7580f739fc16e0615bdc4b97c7
Feng Lei <fenglei108@163.com>
2019-07-18 17:20:44 +0800

    adjust group create window and group min member

--- consensus/group/era.go
+++ consensus/group/era.go
@@ -22,10 +22,10 @@ import (
 )
 
 const (
-	steadyStateBackTrackGap = 5                                                                 //20                                                                // The gap from the present to the steady state
-	roundWindow             = 15                                                                //80                                                                // The window length of each round
+	steadyStateBackTrackGap = 20                                                                 //20                                                                // The gap from the present to the steady state
+	roundWindow             = 80                                                                //80                                                                // The window length of each round
 	eraWindow               = (steadyStateBackTrackGap+roundWindow)*3 + steadyStateBackTrackGap // The window length of group-create routine
-	lifeWindow              = 8 * eraWindow                                                     // The life window of a active group
+	lifeWindow              = 50 * eraWindow                                                     // The life window of a active group
 )
 
 type rRange struct {
