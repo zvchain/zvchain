c3ac4e7640344a2c258b1e364e58b70a28cb2c67
Feng Lei <fenglei108@163.com>
2019-08-09 15:47:01 +0800

    add log back

--- log/init.go
+++ log/init.go
@@ -1,6 +1,7 @@
 package log
 
 import (
+	"github.com/sirupsen/logrus"
 	"net"
 	"os"
 
@@ -37,7 +38,7 @@ func Init()  {
 	TVMLogger = RusPlus.Logger(logsDir + "tvm", MaxFileSize, Level)
 	PerformLogger = RusPlus.Logger(logsDir + "perform", MaxFileSize, Level)
 	ELKLogger = RusPlus.Logger(logsDir + "ELK", MaxFileSize, Level)
-	//ELKLogger.Hooks.Add(logstash().(logrus.Hook))
+	ELKLogger.Hooks.Add(logstash().(logrus.Hook))
 }
 
 func logstash() interface{}{
