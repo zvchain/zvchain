1b7ad5f999fe5484d518f2358292d2fd5483d86b
lavrock <freewenjie@gmail.com>
2019-08-14 19:57:18 +0800

    rename log

--- log/init.go
+++ log/init.go
@@ -8,7 +8,7 @@ import (
 	"github.com/bshuster-repo/logrus-logstash-hook"
 )
 
-func Init()  {
+func Init() {
 
 	RusPlus = New(nil)
 	StdLogger.SetLevel(Level)
@@ -24,24 +24,24 @@ func Init()  {
 		}
 	}
 
-	DefaultLogger = RusPlus.Logger(logsDir + "default", MaxFileSize, Level)
-	ConsensusLogger = RusPlus.Logger(logsDir + "consensus", MaxFileSize, Level)
-	ConsensusStdLogger = RusPlus.Logger(logsDir + "consensus_std", MaxFileSize, Level)
-	CoreLogger = RusPlus.Logger(logsDir + "core", MaxFileSize, Level)
-	BlockSyncLogger = RusPlus.Logger(logsDir + "block_sync", MaxFileSize, Level)
-	GroupLogger = RusPlus.Logger(logsDir + "group", MaxFileSize, Level)
-	MiddlewareLogger = RusPlus.Logger(logsDir + "middleware", MaxFileSize, Level)
-	TxSyncLogger = RusPlus.Logger(logsDir + "tx_sync", MaxFileSize, Level)
-	P2PLogger = RusPlus.Logger(logsDir + "p2p", MaxFileSize, Level)
-	ForkLogger = RusPlus.Logger(logsDir + "fork", MaxFileSize, Level)
-	StatisticsLogger = RusPlus.Logger(logsDir + "statistics", MaxFileSize, Level)
-	TVMLogger = RusPlus.Logger(logsDir + "tvm", MaxFileSize, Level)
-	PerformLogger = RusPlus.Logger(logsDir + "perform", MaxFileSize, Level)
-	ELKLogger = RusPlus.Logger(logsDir + "ELK", MaxFileSize, Level)
+	DefaultLogger = RusPlus.Logger(logsDir+"default", MaxFileSize, Level)
+	ConsensusLogger = RusPlus.Logger(logsDir+"consensus", MaxFileSize, Level)
+	ConsensusStdLogger = RusPlus.Logger(logsDir+"consensus_std", MaxFileSize, Level)
+	CoreLogger = RusPlus.Logger(logsDir+"core", MaxFileSize, Level)
+	BlockSyncLogger = RusPlus.Logger(logsDir+"block_sync", MaxFileSize, Level)
+	GroupLogger = RusPlus.Logger(logsDir+"group", MaxFileSize, Level)
+	MiddlewareLogger = RusPlus.Logger(logsDir+"middleware", MaxFileSize, Level)
+	TxSyncLogger = RusPlus.Logger(logsDir+"tx_sync", MaxFileSize, Level)
+	P2PLogger = RusPlus.Logger(logsDir+"p2p", MaxFileSize, Level)
+	ForkLogger = RusPlus.Logger(logsDir+"fork", MaxFileSize, Level)
+	StatisticsLogger = RusPlus.Logger(logsDir+"statistics", MaxFileSize, Level)
+	TVMLogger = RusPlus.Logger(logsDir+"tvm", MaxFileSize, Level)
+	PerformLogger = RusPlus.Logger(logsDir+"perform", MaxFileSize, Level)
+	ELKLogger = RusPlus.Logger(logsDir+"NetTest", MaxFileSize, Level)
 	ELKLogger.Hooks.Add(logstash().(logrus.Hook))
 }
 
-func logstash() interface{}{
+func logstash() interface{} {
 	conn, err := net.Dial("tcp", "47.252.87.139:5566")
 	if err != nil {
 		panic("")
