5d061cc5c60de8ccb94fb6b3447e27f30d64f371
chenxiaoli <346004279@qq.com>
2019-08-16 17:36:00 +0800

    mysql操作基础数据修改

---
+++ browser/models/models.go
@@ -0,0 +1,36 @@
+package models
+
+//   Copyright (C) 2018 TASChain
+//
+//   This program is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+type Account struct {
+	gorm.Model
+	Address          string `json:"address"`
+	Type             uint64 `json:"type"`
+	RoleType         uint64 `json:"role_type"`
+	ProposalStake    uint64 `json:"proposal_stake"`
+	VerifyStake      uint64 `json:"verify_stake"`
+	OtherStake       uint64 `json:"other_stake"`
+	Group            string `json:"group"`
+	TotalTransaction uint64 `json:"total_transaction"`
+	Rewards          uint64 `json:"rewards"`
+	Status           uint64 `json:"status"`
+	StakeFrom        uint64 `json:"stake_from"`
+	Balance          uint64 `json:"balance"`
+}
---
+++ browser/mysql/storage_excute.go
@@ -0,0 +1,46 @@
+package mysql
+
+import (
+	"fmt"
+	"github.com/zvchain/zvchain/browser/models"
+)
+
+func (storage *Storage) updateAccount(accounts []*models.Account) bool {
+	//fmt.Println("[Storage] add log ")
+	if storage.db == nil {
+		fmt.Println("[Storage] storage.db == nil")
+		return false
+	}
+
+	for i := 0; i < len(accounts); i++ {
+		if accounts[i] != nil {
+			storage.UpdateObject(&accounts[i])
+		}
+	}
+	return true
+}
+
+func (storage *Storage) addAccount(accounts []*models.Account) bool {
+	//fmt.Println("[Storage] add log ")
+	if storage.db == nil {
+		fmt.Println("[Storage] storage.db == nil")
+		return false
+	}
+	for i := 0; i < len(accounts); i++ {
+		if accounts[i] != nil {
+			storage.AddObjects(&accounts[i])
+		}
+	}
+	return true
+}
+
+func (storage *Storage) GetAccountById(id string) []*models.Account {
+	//fmt.Println("[Storage] add Verification ")
+	if storage.db == nil {
+		fmt.Println("[Storage] storage.db == nil")
+		return nil
+	}
+	accounts := make([]*models.Account, 1, 1)
+	storage.db.Where("id = ? ", id).Find(&accounts)
+	return accounts
+}
