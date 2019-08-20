943d9b2ca3b62eabb9aa5703b89190f2ac5984de
chenxiaoli <346004279@qq.com>
2019-08-20 15:47:15 +0800

    test

---
+++ browser/crontab/a_test.go
@@ -0,0 +1,28 @@
+package crontab
+
+import (
+	"fmt"
+	"github.com/zvchain/zvchain/browser/models"
+	"testing"
+)
+
+func TestDB2(t *testing.T) {
+	server := NewServer("10.0.0.13", 3306, "root", "root123", "119.23.68.106", 8101, false)
+	for i := 0; i < 100; i++ {
+		begin := uint64(i * 1000000)
+		end := uint64((i + 1) * 1000000)
+		fmt.Printf("index : begin:%v end:%v\n", begin, end)
+		acc := server.storage.GetAccountById("0x07eafa7c040e9537837e1c3c3580d87633b019c8ae4f9a5b954c5806340e0886")
+		bool := server.storage.UpdateAccountByColumn(acc[0], map[string]interface{}{"proposal_stake": 1,
+			"other_stake":  2,
+			"verify_stake": 3,
+			"stake_from":   "jack"})
+		sys := &models.Sys{
+			Variable: "block_reward.top_block_height",
+			SetBy:    "carrie.cxl",
+		}
+		server.storage.AddBlockRewardSystemconfig(sys)
+		fmt.Println("", bool)
+	}
+
+}
