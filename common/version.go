7adca8cdf764bffdd8a6cc832f2782e6889cae2c
liuhao <feiling111222@gmail.com>
2019-08-22 21:23:52 +0800

    update version

--- common/version.go
+++ common/version.go
@@ -31,10 +31,10 @@ package common
 //const GtasVersion = "0.9.3" // add comments
 //const GtasVersion = "0.9.10" // add comments
 //const GtasVersion = "0.9.11" // fix group member check
-const GtasVersion = "0.9.13"
+const GtasVersion = "0.9.14"
 
 const ConsensusVersion = 1
 
-const ChainDataVersion = 12
+const ChainDataVersion = 13
 
 const ProtocolVersion = 1
