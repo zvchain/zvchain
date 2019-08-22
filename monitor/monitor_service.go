c67c1e9359462ec8d450859cdc68a1c5ace25124
darren0718 <kobep@126.com>
2019-08-22 16:01:24 +0800

    fix update monitor info bug

--- monitor/monitor_service.go
+++ monitor/monitor_service.go
@@ -242,7 +242,7 @@ func (ms *MonitorService) UpdateNodeInfo(ni *NodeInfo) {
 		dm["NType"] = ms.nodeInfo.Type
 		dm["VrfThreshold"] = ms.nodeInfo.VrfThreshold
 		dm["PStake"] = ms.nodeInfo.PStake
-		dm["SaveHeight"] = ms.nodeInfo.BlockHeight
+		dm["BlockHeight"] = ms.nodeInfo.BlockHeight
 		dm["GroupHeight"] = ms.nodeInfo.GroupHeight
 		dm["TxPoolCount"] = ms.nodeInfo.TxPoolCount
 		dm["CPU"] = ms.resStat.CPU
@@ -257,6 +257,8 @@ func (ms *MonitorService) UpdateNodeInfo(ni *NodeInfo) {
 			if affet <= 0 {
 				sess.Table("nodes").Data(dm).Insert()
 			}
+		} else {
+			log.ConsensusStdLogger.Errorf("update node info error:%v", err)
 		}
 	}
 }
