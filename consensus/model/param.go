b803a859debc087443159b20c7b46a74c620996b
lemeng <273341048@qq.com>
2019-10-21 09:49:56 +0800

    reset address

--- consensus/model/param.go
+++ consensus/model/param.go
@@ -34,7 +34,7 @@ const (
 	GroupMaxMembers int = 100
 
 	// GroupMinMembers means the minimum number of members in a group
-	GroupMinMembers int = 80
+	GroupMinMembers int = 10
 )
 
 // ConsensusParam defines all the params of the consensus engine
