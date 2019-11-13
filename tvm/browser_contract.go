3c16804efec5d13028a5debe55628293e7c66c5f
chenxiaoli <346004279@qq.com>
2019-11-13 15:02:57 +0800

    bug fix

--- tvm/browser_contract.go
+++ tvm/browser_contract.go
@@ -2,10 +2,12 @@ package tvm
 
 import (
 	"fmt"
+	"sync"
 )
 
 var ContractTransferData = make(chan *ContractTransfer, 500)
 var MapTokenChan = make(map[string]chan *TokenContractTransfer)
+var lock = new(sync.RWMutex)
 
 type TokenContractTransfer struct {
 	ContractAddr string
@@ -31,13 +33,20 @@ func ProduceTokenContractTransfer(txhash string, blockHash string, contracttoken
 		BlockHash:    blockHash,
 		TxHash:       txhash,
 	}
+	lock.Lock()
 	if MapTokenChan[blockHash] == nil {
 		MapTokenChan[blockHash] = make(chan *TokenContractTransfer, 500)
 	}
-	MapTokenChan[blockHash] <- contract
+	lock.Unlock()
+	setmap(blockHash, contract)
 	//TokenTransferData <- contract
-	fmt.Println("ProduceTokenContractTransfer,addr:", string(addr), "hash:", blockHash, "tx_hash:", txhash, ",contractcode:", contracttoken, "value", contract.Value)
+	fmt.Println("ProduceTokenContractTransfer,addr:", string(addr), "hash:", blockHash, ",contractcode:", contracttoken, "value", contract.Value)
 }
+func setmap(blockHash string, contract *TokenContractTransfer) {
+	MapTokenChan[blockHash] <- contract
+
+}
+
 func ProduceContractTransfer(txHash string,
 	addr string,
 	value uint64,
