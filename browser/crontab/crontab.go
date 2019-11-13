8e22563a603b1813cb35636a278f996994759825
chenxiaoli <346004279@qq.com>
2019-11-13 10:58:59 +0800

    log update

--- browser/crontab/crontab.go
+++ browser/crontab/crontab.go
@@ -555,7 +555,7 @@ func (crontab *Crontab) ConsumeTokenContractTransfer(height uint64, hash string)
 			if !crontab.storage.IsDbTokenContract(data.ContractAddr) {
 				return
 			}
-			browserlog.BrowserLog.Info("ConsumeTokenContractTransfer,json:", data.TxHash, height, hash)
+			browserlog.BrowserLog.Info("ConsumeTokenContractTransfer,json:", data.TxHash, ",", height, ",", hash)
 			chain := core.BlockChainImpl
 			wrapper := chain.GetTransactionPool().GetReceipt(common.HexToHash(data.TxHash))
 			if wrapper != nil {
