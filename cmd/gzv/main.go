ec306db750a443bff9fa85b8887845d4599735bd
chenxiaoli <346004279@qq.com>
2019-12-06 18:21:53 +0800

    confirm max reward upgrade

--- cmd/gzv/main.go
+++ cmd/gzv/main.go
@@ -40,9 +40,9 @@ func main() {
 }
 
 func NewBrowserDBInit() {
-	var browerdbaddr, rpcAddr string
+	var browerdbaddr, rpcAddr, browerslavedbaddr string
 	var dbPort, rpcPort int
-	var dbUser, dbPassword string
+	var dbUser, dbPassword, slavedbUser, slavePassword string
 	var help bool
 	var reset bool
 	var resetcrontab bool
@@ -51,11 +51,15 @@ func NewBrowserDBInit() {
 	flag.BoolVar(&reset, "reset", false, "reset database")
 	flag.BoolVar(&resetcrontab, "resetcrontab", false, "resetcrontab database")
 	flag.StringVar(&browerdbaddr, "browerdbaddr", "localhost", "database address")
+	flag.StringVar(&browerslavedbaddr, "browerslavedbaddr", "localhost", "database address")
+
 	flag.StringVar(&rpcAddr, "rpcaddr", "localhost", "RPC address")
 	flag.IntVar(&dbPort, "dbport", 3306, "database port")
 	flag.IntVar(&rpcPort, "rpcport", 8101, "RPC port")
 	flag.StringVar(&dbUser, "dbuser", "root", "database user")
-	flag.StringVar(&dbPassword, "browerdbpw", "TASchain@1003", "database password")
+	flag.StringVar(&slavedbUser, "slavedbUser", "root", "database user")
+	flag.StringVar(&slavePassword, "slavePassword", "root", "database password")
+	flag.StringVar(&dbPassword, "browerdbpw", "root", "database password")
 
 	/*
 		// for local test
@@ -73,7 +77,6 @@ func NewBrowserDBInit() {
 	browserlog.InitLog()
 	ldb.InitBrowserdb()
 	fmt.Println("browserdbmmanagement flags:", browerdbaddr, dbPort, dbUser, dbPassword, reset)
-
 	browser.NewDBMmanagement(browerdbaddr, dbPort, dbUser, dbPassword, reset, resetcrontab)
-	crontab.NewServer(browerdbaddr, dbPort, dbUser, dbPassword, reset)
+	crontab.NewServer(browerdbaddr, dbPort, dbUser, dbPassword, reset, browerslavedbaddr, slavedbUser, slavePassword)
 }
