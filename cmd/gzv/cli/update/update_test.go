143428bf164a98e10b166a1676db1a5d76b2aba5
sjxad <15215616197@163.com>
2019-12-09 15:39:07 +0800

    delete test request url

--- cmd/gzv/cli/update/update_test.go
+++ cmd/gzv/cli/update/update_test.go
@@ -1,16 +1,11 @@
 package update
 
-import (
-	"fmt"
-	"testing"
-)
-
-func TestRequest(t *testing.T) {
-	RequestUrl = "http://47.110.159.248:8000/request"
-	vc := NewVersionChecker()
-	no, err := vc.requestVersion()
-	if err != nil {
-		t.Error("err :", err)
-	}
-	fmt.Println("no ===>", no)
-}
+//func TestRequest(t *testing.T) {
+//	RequestUrl = "http://47.110.159.248:8000/request"
+//	vc := NewVersionChecker()
+//	no, err := vc.requestVersion()
+//	if err != nil {
+//		t.Error("err :", err)
+//	}
+//	fmt.Println("no ===>", no)
+//}
