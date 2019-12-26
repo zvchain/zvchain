1192238fa9e1a31e257d14e502bc3294dfc261c7
darren0718 <kobep@126.com>
2019-12-26 17:09:04 +0800

    prune test

--- core/state_prune_test.go
+++ core/state_prune_test.go
@@ -16,14 +16,83 @@
 package core
 
 import (
+	"fmt"
+	"github.com/zvchain/zvchain/common"
+	"github.com/zvchain/zvchain/middleware/types"
+	"github.com/zvchain/zvchain/storage/trie"
+	"log"
 	"testing"
+	"time"
 )
 
-func TestTraverseGroupAfterPrune(t *testing.T) {
-	//group := newGrou
-	//tailor, err := NewOfflineTailor(&types.GenesisInfo{}, "", "", 1024, "", "", true)
-	//if err != nil {
-	//	t.Error(err)
-	//}
+func traverseGroup(db types.AccountDB, seed common.Hash, cachedb *trie.NodeDatabase, onlyTraverKey []byte) error {
+	obj := db.GetStateObject(common.HashToAddress(seed))
+	if obj == nil {
+		return fmt.Errorf("seed address object not exist")
+	}
+	root := obj.GetRootHash()
+	trie, err := trie.NewTrie(root, cachedb)
+	if err != nil {
+		return err
+	}
 
+	var (
+		ok bool
+	)
+
+	if len(onlyTraverKey) == 0 {
+		ok, err = trie.Traverse(func(key []byte, value []byte) error {
+			log.Printf("all traverse seed %v, key %v, valuesize %v", seed, string(key), len(value))
+			return nil
+		}, nil, false)
+	} else {
+		ok, err = trie.TraverseKey(onlyTraverKey, func(key []byte, value []byte) error {
+			log.Printf("only key traverse seed %v, key %v, valuesize %v", seed, string(key), len(value))
+			return nil
+		}, nil, false)
+	}
+	if !ok {
+		return err
+	}
+
+	return nil
+}
+
+func TestTraverseGroupAfterPrune(t *testing.T) {
+	group := newGroup4CPTest(0, common.MaxUint64)
+	tailor, err := NewOfflineTailor(&types.GenesisInfo{Group: group}, "", "", 1024, "", "", true, 1024)
+	if err != nil {
+		t.Error(err)
+	}
+	height := tailor.chain.Height()
+	fmt.Println("height ", height)
+	db, err := tailor.chain.AccountDBAt(height)
+	if err != nil {
+		t.Fatalf("get db error at %v, err %v", height, err)
+	}
+
+	groupSeeds, err := tailor.groupReader.GetAllGroupSeedsByHeight(height)
+	if err != nil {
+		t.Fatalf("load group seed error %v", err)
+	}
+	log.Printf("group size %v", len(groupSeeds))
+
+	var traverseKey []byte
+
+	for i := 0; i < len(groupSeeds); {
+		b := time.Now()
+		seed := groupSeeds[i]
+		if err = traverseGroup(db, seed, tailor.chain.stateCache.TrieDB(), traverseKey); err != nil {
+			if isMissingNodeError(err) && traverseKey == nil {
+				traverseKey = tailor.groupReader.GroupKey()
+				log.Printf("traverse group %v missing node, start traverse key %v", seed, traverseKey)
+				continue
+			} else {
+				t.Fatal(err)
+			}
+		}
+		i++
+		log.Printf("traverse group %v success, cost %v", seed, time.Since(b).String())
+	}
+	t.Log("success")
 }
