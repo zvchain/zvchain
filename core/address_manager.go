3b7110b2a67babcb8d5b28fe02ffd72c16d627f0
chenxiaoli <346004279@qq.com>
2020-07-15 10:55:18 +0800

    address manager

--- core/address_manager.go
+++ core/address_manager.go
@@ -12,9 +12,8 @@ import (
 	"sync"
 )
 
-const AddressManagerContract = "zv1388dee8f36656bff16a886570ed8967e1d378d73309317e9b70bfb6ed3c1be8"
-const AddressManagerContractTest = "zv07ef5f16247334a2076f8348db719a0115c7c2904403377ae93c7b8fd2827fa2"
-const Nonce = 5
+const AddressContract = "zv0000000000000000000000000000000000000000000000000000000000000001"
+const AddressSource = "0x0001"
 
 func isUseContract() bool {
 	chain := BlockChainImpl
@@ -29,95 +28,91 @@ func isUseContract() bool {
 }
 
 func DaemonNodeAddress() common.Address {
-	if isUseContract() && addressManager.daemonNodeAddr != nil {
-		fmt.Println("DaemonNodeAddress,", addressManager.daemonNodeAddr.AddrPrefixString())
-		return *addressManager.daemonNodeAddr
+	if isUseContract() {
+		daemonNodeAddr := loadNormalAddress("daemonNodeAddr")
+		if daemonNodeAddr != nil {
+			fmt.Println("daemonNodeAddr,", daemonNodeAddr)
+			return *daemonNodeAddr
+		}
 	}
 	return types.DaemonNodeAddress()
 }
 
 func UserNodeAddress() common.Address {
-	if isUseContract() && addressManager.userNodeAddr != nil {
-		fmt.Println("UserNodeAddress,", addressManager.userNodeAddr.AddrPrefixString())
-		return *addressManager.userNodeAddr
+	if isUseContract() {
+		userNodeAddr := loadNormalAddress("userNodeAddr")
+		if userNodeAddr != nil {
+			fmt.Println("userNodeAddr,", userNodeAddr)
+			return *userNodeAddr
+		}
 	}
 	return types.UserNodeAddress()
 }
 
 func CirculatesAddr() common.Address {
-	if isUseContract() && addressManager.circulatesAddr != nil {
-		fmt.Println("circulatesAddr,", addressManager.circulatesAddr.AddrPrefixString())
-		return *addressManager.circulatesAddr
+	if isUseContract() {
+		circulatesAddr := loadNormalAddress("circulatesAddr")
+		if circulatesAddr != nil {
+			fmt.Println("circulatesAddr,", circulatesAddr)
+			return *circulatesAddr
+		}
 	}
 	return types.CirculatesAddr()
 }
 
 func StakePlatformAddr() common.Address {
-	if isUseContract() && addressManager.stakePlatformAddr != nil {
-		fmt.Println("stakePlatformAddr,", addressManager.stakePlatformAddr.AddrPrefixString())
-		return *addressManager.stakePlatformAddr
+	if isUseContract() {
+		stakePlatformAddr := loadNormalAddress("stakePlatformAddr")
+		if stakePlatformAddr != nil {
+			fmt.Println("stakePlatformAddr,", stakePlatformAddr)
+			return *stakePlatformAddr
+		}
 	}
 	return types.StakePlatformAddr()
 }
 
 func AdminAddr() common.Address {
-	if isUseContract() && addressManager.adminAddr != nil {
-		fmt.Println("AdminAddr,", addressManager.adminAddr.AddrPrefixString())
-		return *addressManager.adminAddr
+	if isUseContract() {
+
+		adminAddr := loadNormalAddress("adminAddr")
+		if adminAddr != nil {
+			fmt.Println("AdminAddr,", adminAddr)
+			return *adminAddr
+		}
 	}
 	return types.AdminAddr()
 }
 
 func GuardAddress() []common.Address {
-	if isUseContract() && addressManager.GuardNodes != nil {
-		fmt.Println("GuardNodes[0],", addressManager.GuardNodes[0])
-		return addressManager.GuardNodes
+	if isUseContract() {
+		guardAddress := loadGuardAddress()
+		if guardAddress != nil && len(guardAddress) > 0 {
+			fmt.Println("GuardNodes[0],", guardAddress[0])
+			return guardAddress
+
+		}
 	}
 	return types.GuardAddress()
 }
 
 type AddressManager struct {
-	adminAddr         *common.Address
-	stakePlatformAddr *common.Address
-	circulatesAddr    *common.Address
-	userNodeAddr      *common.Address
-	daemonNodeAddr    *common.Address
-	GuardNodes        []common.Address
-	ContractAddr      *common.Address
-	//TestContractAddr  common.Address
-	cacheBlockHash common.Hash
+	deployContract bool
 	mu             sync.Mutex
 }
 
 var addressManager AddressManager
 
-func AddressContractAddr() string {
-	if types.IsNormalChain() {
-		return AddressManagerContract
-	}
-	return AddressManagerContractTest
-}
-
 func (am *AddressManager) CheckAndUpdate() {
 	am.mu.Lock()
 	defer am.mu.Unlock()
-	chain := BlockChainImpl
-	if am.cacheBlockHash == chain.QueryTopBlock().Hash {
+	if am.deployContract {
 		return
 	}
-	am.cacheBlockHash = chain.QueryTopBlock().Hash
-	if am.ContractAddr == nil {
-		if params.GetChainConfig().EqualZIP005(chain.QueryTopBlock().Height) {
-			am.deployAddressManagerContract()
-		} else {
-			genesisInfo := chain.consensusHelper.GenerateGenesisInfo()
-			adminAddr := common.BytesToAddress(genesisInfo.Group.Members()[0].ID())
-			addr := am.getContractAddr(adminAddr, Nonce)
-			fmt.Println("get_adminAddraddress,", addr.AddrPrefixString())
-			am.ContractAddr = &addr
-		}
+	chain := BlockChainImpl
+	am.deployContract = true
+	if params.GetChainConfig().EqualZIP005(chain.QueryTopBlock().Height) {
+		am.deployAddressManagerContract()
 	}
-	am.loadAllAddress()
 }
 
 func (am *AddressManager) deployAddressManagerContract() {
@@ -129,21 +124,14 @@ func (am *AddressManager) deployAddressManagerContract() {
 	am.autoCreateContract(contractName, codeStr)
 }
 
-func (am *AddressManager) getContractAddr(source common.Address, nonce uint64) common.Address {
-	contractAddress := common.BytesToAddress(common.Sha256(common.BytesCombine(source[:], common.Uint64ToByte(nonce))))
-	return contractAddress
-}
-
 func (am *AddressManager) autoCreateContract(contractName string, contractCode string) *common.Address {
 	chain := BlockChainImpl
 	stateDB, _ := chain.LatestAccountDB()
 	txRaw := &types.RawTransaction{}
-	genesisInfo := chain.consensusHelper.GenerateGenesisInfo()
-	adminAddr := common.BytesToAddress(genesisInfo.Group.Members()[0].ID())
+	adminAddr := common.StringToAddress(AddressSource)
 	txRaw.Source = &adminAddr
 	txRaw.Value = &types.BigInt{Int: *big.NewInt(0)}
 	txRaw.GasLimit = &types.BigInt{Int: *big.NewInt(300000)}
-	txRaw.Nonce = Nonce
 	controller := tvm.NewController(stateDB, nil, nil, types.NewTransaction(txRaw, txRaw.GenHash()), 0, nil)
 	contract := tvm.Contract{
 		Code:         contractCode,
@@ -153,79 +141,106 @@ func (am *AddressManager) autoCreateContract(contractName string, contractCode s
 	if err != nil {
 		panic(fmt.Sprintf("jsonMarshal Addressmanagercontract error: %s", err.Error()))
 	}
-	contractAddress := am.getContractAddr(adminAddr, Nonce)
+	contractAddress := common.StringToAddress(AddressContract)
 	fmt.Println("get_create_adminAddraddress,", contractAddress.AddrPrefixString())
 	stateDB.CreateAccount(contractAddress)
 	stateDB.SetCode(contractAddress, jsonBytes)
 
 	contract.ContractAddress = &contractAddress
 	controller.VM.SetGas(500000)
-	_, _, transactionError := controller.Deploy(&contract)
+	result, _, transactionError := controller.Deploy(&contract)
+	re, _ := json.Marshal(&result)
+	fmt.Println("controller.Deploy,", string(re))
 	if transactionError != nil {
 		panic(fmt.Sprintf("deploy Addressmanagercontract error: %s", transactionError.Message))
 	}
-	am.ContractAddr = &contractAddress
+	//am.ContractAddr = &contractAddress
 	return contract.ContractAddress
 }
 
-func (am *AddressManager) loadAllAddress() bool {
+func loadGuardAddress() []common.Address {
 
-	am.GuardNodes = make([]common.Address, 0, 0)
+	guardNodes := make([]common.Address, 0, 0)
 	chain := BlockChainImpl
 	db, err := chain.LatestAccountDB()
 	if err != nil {
 		fmt.Errorf("address manager loadAllAddress err:%v ", err)
-		return false
+		return nil
 	}
-	genesisInfo := chain.consensusHelper.GenerateGenesisInfo()
-	adminAddr := common.BytesToAddress(genesisInfo.Group.Members()[0].ID())
-	nonce := db.GetNonce(adminAddr)
-	if nonce != 0 {
-		nonce += 1
-	}
-	contractAddress := am.ContractAddr
-
-	iter := db.DataIterator(*contractAddress, []byte{})
+	contractAddress := common.StringToAddress(AddressContract)
+	iter := db.DataIterator(contractAddress, []byte{})
 	if iter == nil {
 		fmt.Errorf("address manager loadAllAddress err,iter is nil ")
-		return false
+		return nil
 	}
 
 	for iter.Next() {
 		k := string(iter.Key[:])
 		if strings.HasPrefix(k, "pool_lists@") {
 			addr := strings.TrimLeft(k, "pool_lists@")
-			am.GuardNodes = append(am.GuardNodes, common.StringToAddress(addr))
-		} else {
+			guardNodes = append(guardNodes, common.StringToAddress(addr))
+		}
+	}
+	return guardNodes
+}
+
+func loadNormalAddress(key string) *common.Address {
+	chain := BlockChainImpl
+	db, err := chain.LatestAccountDB()
+	if err != nil {
+		fmt.Errorf("address manager loadAllAddress err:%v ", err)
+		return nil
+	}
+	contractAddress := common.StringToAddress(AddressContract)
+	iter := db.DataIterator(contractAddress, []byte{})
+	if iter == nil {
+		fmt.Errorf("address manager loadAllAddress err,iter is nil ")
+		return nil
+	}
+	for iter.Next() {
+		k := string(iter.Key[:])
+		if !strings.HasPrefix(k, "pool_lists@") {
 			v := tvm.VmDataConvert(iter.Value[:])
+			resultAddr := &common.Address{}
 			switch k {
 			case "adminAddr":
 				if addr, ok := v.(string); ok {
 					adminAddr := common.StringToAddress(addr)
-					am.adminAddr = &adminAddr
+					resultAddr = &adminAddr
 				}
+
 			case "stakePlatformAddr":
 				if addr, ok := v.(string); ok {
 					stakePlatformAddr := common.StringToAddress(addr)
-					am.stakePlatformAddr = &stakePlatformAddr
+					//am.stakePlatformAddr = &stakePlatformAddr
+					resultAddr = &stakePlatformAddr
+
 				}
 			case "circulatesAddr":
 				if addr, ok := v.(string); ok {
 					circulatesAddr := common.StringToAddress(addr)
-					am.circulatesAddr = &circulatesAddr
+					resultAddr = &circulatesAddr
+
+					//am.circulatesAddr = &circulatesAddr
 				}
 			case "userNodeAddr":
 				if addr, ok := v.(string); ok {
 					userNodeAddr := common.StringToAddress(addr)
-					am.userNodeAddr = &userNodeAddr
+					resultAddr = &userNodeAddr
+
+					//am.userNodeAddr = &userNodeAddr
 				}
 			case "daemonNodeAddr":
 				if addr, ok := v.(string); ok {
 					daemonNodeAddr := common.StringToAddress(addr)
-					am.daemonNodeAddr = &daemonNodeAddr
+					resultAddr = &daemonNodeAddr
+					//am.daemonNodeAddr = &daemonNodeAddr
 				}
+			}
+			if key == k {
+				return resultAddr
 			}
 		}
 	}
-	return true
+	return nil
 }
